53c0e9b2d28f40311bd51586a8a932140abe3de4
Jenkins <jenkins@kaltura.com>
2019-02-18 10:43:03 +0000

    Auto-generated by Jenkins

--- KalturaClient.js
+++ KalturaClient.js
@@ -598,7 +598,7 @@ var MD5 = function (string) {
 function KalturaClient(config){
 	this.init(config);
 	this.setClientTag('ajax:19-02-18');
-	this.setApiVersion('5.1.2.17543');
+	this.setApiVersion('5.1.2.19212');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
