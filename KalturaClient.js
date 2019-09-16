79dcbd7f82c433a9998ce62ca5ba6f658b15ecaf
Backend CI <ott.rnd.core@kaltura.com>
2019-09-16 07:58:44 +0000

    Auto-generated by Jenkins

--- KalturaClient.js
+++ KalturaClient.js
@@ -597,8 +597,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:19-09-15');
-	this.setApiVersion('5.2.6.13600');
+	this.setClientTag('ajax:19-09-16');
+	this.setApiVersion('5.2.6.5992');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
