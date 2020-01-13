e3f435a1c87558a4970d021675913e722b1cf93f
Backend CI <ott.rnd.core@kaltura.com>
2020-01-13 17:34:41 +0000

    Auto-generated by Jenkins

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -4866,16 +4866,16 @@ var KalturaSubscriptionSetService = {
 var KalturaSystemService = {
 	/**
 	 * Clear local server cache.
-	 * @param	action	string		action to perform, possible values: clear_all / keys / getKey (optional, default: null)
+	 * @param	clearCacheAction	string		clear cache action to perform, possible values: clear_all / keys / getKey (optional, default: null)
 	 * @param	key	string		key to get in case you send action getKey (optional, default: null)
 	 **/
-	clearLocalServerCache: function(action, key){
-		if(!action)
-			action = null;
+	clearLocalServerCache: function(clearCacheAction, key){
+		if(!clearCacheAction)
+			clearCacheAction = null;
 		if(!key)
 			key = null;
 		var kparams = new Object();
-		kparams.action = action;
+		kparams.clearCacheAction = clearCacheAction;
 		kparams.key = key;
 		return new KalturaRequestBuilder("system", "clearLocalServerCache", kparams);
 	},
@@ -6205,8 +6205,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:20-01-09');
-	this.setApiVersion('5.3.1.14539');
+	this.setClientTag('ajax:20-01-13');
+	this.setApiVersion('5.3.1.14545');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
