17dc9da2377ef274165d189d9fb8db8bb44b736d
Backend CI <ott.rnd.core@kaltura.com>
2021-05-10 12:07:01 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/459, branch master

--- KalturaPriceDetailsService.js
+++ KalturaPriceDetailsService.js
@@ -4,26 +4,6 @@
  **/
 var KalturaPriceDetailsService = {
 	/**
-	 * Internal API !!! Insert new PriceDetails for partner.
-	 * @param	priceDetails	KalturaPriceDetails		PriceDetails Object (optional)
-	 **/
-	add: function(priceDetails){
-		var kparams = new Object();
-		kparams.priceDetails = priceDetails;
-		return new KalturaRequestBuilder("pricedetails", "add", kparams);
-	},
-	
-	/**
-	 * Internal API !!! Delete PriceDetails.
-	 * @param	id	int		PriceDetails identifier (optional)
-	 **/
-	deleteAction: function(id){
-		var kparams = new Object();
-		kparams.id = id;
-		return new KalturaRequestBuilder("pricedetails", "delete", kparams);
-	},
-	
-	/**
 	 * Returns the list of available prices, can be filtered by price IDs.
 	 * @param	filter	KalturaPriceDetailsFilter		Filter (optional, default: null)
 	 **/
