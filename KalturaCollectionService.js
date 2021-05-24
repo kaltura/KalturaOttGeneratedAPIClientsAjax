85cc7a5bfc8443cd2a5422214264fd348017f497
Backend CI <ott.rnd.core@kaltura.com>
2021-05-24 10:32:55 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/475, branch partnerAddAndPriceDetailes_PPV

--- KalturaCollectionService.js
+++ KalturaCollectionService.js
@@ -4,6 +4,26 @@
  **/
 var KalturaCollectionService = {
 	/**
+	 * Internal API !!! Insert new collection for partner.
+	 * @param	collection	KalturaCollection		collection object (optional)
+	 **/
+	add: function(collection){
+		var kparams = new Object();
+		kparams.collection = collection;
+		return new KalturaRequestBuilder("collection", "add", kparams);
+	},
+	
+	/**
+	 * Internal API !!! Delete collection.
+	 * @param	id	int		Collection id (optional)
+	 **/
+	deleteAction: function(id){
+		var kparams = new Object();
+		kparams.id = id;
+		return new KalturaRequestBuilder("collection", "delete", kparams);
+	},
+	
+	/**
 	 * Returns a list of subscriptions requested by Subscription ID or file ID.
 	 * @param	filter	KalturaCollectionFilter		Filter request (optional, default: null)
 	 * @param	pager	KalturaFilterPager		Page size and index (optional, default: null)
