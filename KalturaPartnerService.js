e13a3f7c2f583644f186a3e9e48c6b7c60ab89ba
Backend CI <ott.rnd.core@kaltura.com>
2021-06-17 12:19:38 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/506, branch 6_4_0_2

--- KalturaPartnerService.js
+++ KalturaPartnerService.js
@@ -4,35 +4,10 @@
  **/
 var KalturaPartnerService = {
 	/**
-	 * Add a partner with default user.
-	 * @param	partner	KalturaPartner		partner (optional)
-	 * @param	partnerSetup	KalturaPartnerSetup		mandatory parameters to create partner (optional)
-	 **/
-	add: function(partner, partnerSetup){
-		var kparams = new Object();
-		kparams.partner = partner;
-		kparams.partnerSetup = partnerSetup;
-		return new KalturaRequestBuilder("partner", "add", kparams);
-	},
-	
-	/**
 	 * Returns a login session for external system (like OVP).
 	 **/
 	externalLogin: function(){
 		var kparams = new Object();
 		return new KalturaRequestBuilder("partner", "externalLogin", kparams);
-	},
-	
-	/**
-	 * Internal API !!! Returns the list of active Partners.
-	 * @param	filter	KalturaPartnerFilter		Filter (optional, default: null)
-	 **/
-	listAction: function(filter){
-		if(!filter)
-			filter = null;
-		var kparams = new Object();
-		if (filter != null)
-			kparams.filter = filter;
-		return new KalturaRequestBuilder("partner", "list", kparams);
 	}
 }
