ab89ff143dc0d25ea0beaa7c52c4f2eeb7cf0e94
Backend CI <ott.rnd.core@kaltura.com>
2021-08-23 14:11:59 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/592, branch BEO-10565_add_filtering_by_linear

--- KalturaClient.js
+++ KalturaClient.js
@@ -597,8 +597,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:21-06-30');
-	this.setApiVersion('6.5.0.29184');
+	this.setClientTag('ajax:21-08-23');
+	this.setApiVersion('6.7.0.29269');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
