0c115c0b5b186052421036c88c2d08d321194e74
Backend CI <ott.rnd.core@kaltura.com>
2022-02-14 12:24:50 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/791, branch master

--- KalturaClient.js
+++ KalturaClient.js
@@ -597,8 +597,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:22-02-02');
-	this.setApiVersion('7.2.0.29783');
+	this.setClientTag('ajax:22-02-14');
+	this.setApiVersion('7.2.0.29784');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -5051,61 +5051,6 @@ var KalturaProductPriceService = {
 }
 
 /**
- *Class definition for the Kaltura service: programAssetGroupOffer.
- **/
-var KalturaProgramAssetGroupOfferService = {
-	/**
-	 * Insert new ProgramAssetGroupOffer for partner.
-	 * @param	programAssetGroupOffer	KalturaProgramAssetGroupOffer		programAssetGroupOffer object (optional)
-	 **/
-	add: function(programAssetGroupOffer){
-		var kparams = new Object();
-		kparams.programAssetGroupOffer = programAssetGroupOffer;
-		return new KalturaRequestBuilder("programassetgroupoffer", "add", kparams);
-	},
-	
-	/**
-	 * Delete programAssetGroupOffer.
-	 * @param	id	int		ProgramAssetGroupOffer id (optional)
-	 **/
-	deleteAction: function(id){
-		var kparams = new Object();
-		kparams.id = id;
-		return new KalturaRequestBuilder("programassetgroupoffer", "delete", kparams);
-	},
-	
-	/**
-	 * Gets all Program asset group offer.
-	 * @param	filter	KalturaProgramAssetGroupOfferFilter		Filter (optional, default: null)
-	 * @param	pager	KalturaFilterPager		Pager (optional, default: null)
-	 **/
-	listAction: function(filter, pager){
-		if(!filter)
-			filter = null;
-		if(!pager)
-			pager = null;
-		var kparams = new Object();
-		if (filter != null)
-			kparams.filter = filter;
-		if (pager != null)
-			kparams.pager = pager;
-		return new KalturaRequestBuilder("programassetgroupoffer", "list", kparams);
-	},
-	
-	/**
-	 * Update ProgramAssetGroupOffer.
-	 * @param	id	int		ProgramAssetGroupOffer id (optional)
-	 * @param	programAssetGroupOffer	KalturaProgramAssetGroupOffer		ProgramAssetGroupOffer (optional)
-	 **/
-	update: function(id, programAssetGroupOffer){
-		var kparams = new Object();
-		kparams.id = id;
-		kparams.programAssetGroupOffer = programAssetGroupOffer;
-		return new KalturaRequestBuilder("programassetgroupoffer", "update", kparams);
-	}
-}
-
-/**
  *Class definition for the Kaltura service: purchaseSettings.
  **/
 var KalturaPurchaseSettingsService = {
@@ -7790,8 +7735,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:22-02-02');
-	this.setApiVersion('7.2.0.29783');
+	this.setClientTag('ajax:22-02-14');
+	this.setApiVersion('7.2.0.29784');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
