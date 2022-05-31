c1b7aee914d5fbee383e3c88c64dd1237900c0f1
Backend CI <ott.rnd.core@kaltura.com>
2022-05-31 07:08:08 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/907, branch BEO-11718_l2v_api

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -3623,6 +3623,57 @@ var KalturaLineupService = {
 }
 
 /**
+ *Class definition for the Kaltura service: liveToVod.
+ **/
+var KalturaLiveToVodService = {
+	/**
+	 * Get existing L2V configuration for both the partner level and all channels level..
+	 **/
+	getConfiguration: function(){
+		var kparams = new Object();
+		return new KalturaRequestBuilder("livetovod", "getConfiguration", kparams);
+	},
+	
+	/**
+	 * Get existing L2V configuration for a specific linear asset..
+	 * @param	linearAssetId	int		Linear asset's identifier. (optional)
+	 **/
+	getLinearAssetConfiguration: function(linearAssetId){
+		var kparams = new Object();
+		kparams.linearAssetId = linearAssetId;
+		return new KalturaRequestBuilder("livetovod", "getLinearAssetConfiguration", kparams);
+	},
+	
+	/**
+	 * Get existing L2V partner configuration..
+	 **/
+	getPartnerConfiguration: function(){
+		var kparams = new Object();
+		return new KalturaRequestBuilder("livetovod", "getPartnerConfiguration", kparams);
+	},
+	
+	/**
+	 * Set L2V configuration for a specific Linear channel..
+	 * @param	configuration	KalturaLiveToVodLinearAssetConfiguration		Live to VOD linear asset (live channel) configuration object. (optional)
+	 **/
+	updateLinearAssetConfiguration: function(configuration){
+		var kparams = new Object();
+		kparams.configuration = configuration;
+		return new KalturaRequestBuilder("livetovod", "updateLinearAssetConfiguration", kparams);
+	},
+	
+	/**
+	 * Set L2V configuration on the partner level..
+	 * @param	configuration	KalturaLiveToVodPartnerConfiguration		Live to VOD configuration object. (optional)
+	 **/
+	updatePartnerConfiguration: function(configuration){
+		var kparams = new Object();
+		kparams.configuration = configuration;
+		return new KalturaRequestBuilder("livetovod", "updatePartnerConfiguration", kparams);
+	}
+}
+
+/**
  *Class definition for the Kaltura service: mediaConcurrencyRule.
  **/
 var KalturaMediaConcurrencyRuleService = {
@@ -7877,8 +7928,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:22-05-19');
-	this.setApiVersion('7.6.0.29891');
+	this.setClientTag('ajax:22-05-31');
+	this.setApiVersion('7.6.0.29902');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
