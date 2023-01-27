8a09a2a4df639477cb156f519bd73044296ea35f
Backend CI <ott.rnd.core@kaltura.com>
2023-01-27 08:19:14 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1041, branch 851_BEO-13368_opc-autocomplete

--- KalturaClient.js
+++ KalturaClient.js
@@ -597,8 +597,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:22-12-25');
-	this.setApiVersion('8.3.1.30102');
+	this.setClientTag('ajax:23-01-27');
+	this.setApiVersion('8.4.0.30117');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -3815,6 +3815,61 @@ var KalturaMediaFileService = {
 }
 
 /**
+ *Class definition for the Kaltura service: mediaFileDynamicData.
+ **/
+var KalturaMediaFileDynamicDataService = {
+	/**
+	 * Add a dynamicData value to the values list of a specific key name in a specific mediaFileTypeId.
+	 * @param	dynamicData	KalturaMediaFileDynamicData		DynamicData value (optional)
+	 **/
+	add: function(dynamicData){
+		var kparams = new Object();
+		kparams.dynamicData = dynamicData;
+		return new KalturaRequestBuilder("mediafiledynamicdata", "add", kparams);
+	},
+	
+	/**
+	 * Delete an existing DynamicData value.
+	 * @param	id	int		DynamicData identifier (optional)
+	 **/
+	deleteAction: function(id){
+		var kparams = new Object();
+		kparams.id = id;
+		return new KalturaRequestBuilder("mediafiledynamicdata", "delete", kparams);
+	},
+	
+	/**
+	 * List and filter existing mediaFile dynamicData values.
+	 * @param	filter	KalturaMediaFileDynamicDataFilter		Filter (optional, default: null)
+	 * @param	pager	KalturaFilterPager		Pager (optional, default: null)
+	 **/
+	listAction: function(filter, pager){
+		if(!filter)
+			filter = null;
+		if(!pager)
+			pager = null;
+		var kparams = new Object();
+		if (filter != null)
+			kparams.filter = filter;
+		if (pager != null)
+			kparams.pager = pager;
+		return new KalturaRequestBuilder("mediafiledynamicdata", "list", kparams);
+	},
+	
+	/**
+	 * Update an existing dynamicData value.
+	 * @param	id	int		DynamicData identifier (optional)
+	 * @param	dynamicData	KalturaMediaFileDynamicData		DynamicData value (optional)
+	 **/
+	update: function(id, dynamicData){
+		var kparams = new Object();
+		kparams.id = id;
+		kparams.dynamicData = dynamicData;
+		return new KalturaRequestBuilder("mediafiledynamicdata", "update", kparams);
+	}
+}
+
+/**
  *Class definition for the Kaltura service: mediaFileType.
  **/
 var KalturaMediaFileTypeService = {
@@ -8042,8 +8097,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:22-12-25');
-	this.setApiVersion('8.3.1.30102');
+	this.setClientTag('ajax:23-01-27');
+	this.setApiVersion('8.4.0.30117');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
