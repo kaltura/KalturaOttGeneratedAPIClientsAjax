806ceee70c69f7e0be9f4875ed8e068f93aabf2a
Backend CI <ott.rnd.core@kaltura.com>
2023-03-07 10:23:47 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1074, branch BEO-13521_Segmentation_AssetUserRuleId

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -8087,8 +8087,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:23-02-14');
-	this.setApiVersion('8.5.1.30206');
+	this.setClientTag('ajax:23-03-07');
+	this.setApiVersion('8.5.1.30283');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
