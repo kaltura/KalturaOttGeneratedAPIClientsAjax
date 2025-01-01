4420a22e8b78800adae6753e46949e70e1fd5770
Backend CI <ott.rnd.core@kaltura.com>
2025-01-01 09:01:56 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1388, branch 10.7.1.4

--- KalturaClient.js
+++ KalturaClient.js
@@ -597,8 +597,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:24-01-15');
-	this.setApiVersion('9.6.0.0');
+	this.setClientTag('ajax:25-01-01');
+	this.setApiVersion('10.7.1.4');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
