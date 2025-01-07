beeb2c3883ae1f4a44e8f22abefb618662e0f02a
Backend CI <ott.rnd.core@kaltura.com>
2025-01-07 17:19:59 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1402, branch BEO-16562

--- KalturaClient.js
+++ KalturaClient.js
@@ -598,7 +598,7 @@ var MD5 = function (string) {
 function KalturaClient(config){
 	this.init(config);
 	this.setClientTag('ajax:25-01-07');
-	this.setApiVersion('10.7.1.4');
+	this.setApiVersion('10.8.0.0');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -381,6 +381,76 @@ var KalturaAssetService = {
 }
 
 /**
+ *Class definition for the Kaltura service: assetEnrich.
+ **/
+var KalturaAssetEnrichService = {
+	/**
+	 * Initiate the process of metadata generation.
+	 * @param	captionUploadJobId	int		job id to generate metadata for (optional)
+	 * @param	externalAssetIds	array		external asset ids (optional)
+	 * @param	targetDisplayLanguage	string		relevant language (optional)
+	 **/
+	generateMetadata: function(captionUploadJobId, externalAssetIds, targetDisplayLanguage){
+		var kparams = new Object();
+		kparams.captionUploadJobId = captionUploadJobId;
+		kparams.externalAssetIds = externalAssetIds;
+		kparams.targetDisplayLanguage = targetDisplayLanguage;
+		return new KalturaRequestBuilder("assetenrich", "generateMetadata", kparams);
+	},
+	
+	/**
+	 * retrieve the status of the metadata generation job.
+	 * @param	captionUploadJobId	int		job id to get (optional)
+	 **/
+	getCaptionUploadJob: function(captionUploadJobId){
+		var kparams = new Object();
+		kparams.captionUploadJobId = captionUploadJobId;
+		return new KalturaRequestBuilder("assetenrich", "getCaptionUploadJob", kparams);
+	},
+	
+	/**
+	 * retrieve the generated metadata.
+	 * @param	captionUploadJobId	int		job id (optional)
+	 **/
+	getGeneratedMetadata: function(captionUploadJobId){
+		var kparams = new Object();
+		kparams.captionUploadJobId = captionUploadJobId;
+		return new KalturaRequestBuilder("assetenrich", "getGeneratedMetadata", kparams);
+	},
+	
+	/**
+	 * retrieve feature configuration.
+	 **/
+	getPartnerConfiguration: function(){
+		var kparams = new Object();
+		return new KalturaRequestBuilder("assetenrich", "getPartnerConfiguration", kparams);
+	},
+	
+	/**
+	 * update feature configuration.
+	 * @param	configuration	KalturaMetaEnrichConfiguration		the partner configuration to be set (optional)
+	 **/
+	updatePartnerConfiguration: function(configuration){
+		var kparams = new Object();
+		kparams.configuration = configuration;
+		return new KalturaRequestBuilder("assetenrich", "updatePartnerConfiguration", kparams);
+	},
+	
+	/**
+	 * Add a file to be used for enriching the assets&#39; metadata.
+	 * @param	json	KalturaCaptionUploadData		Properties of the caption file to be uploaded (optional)
+	 * @param	fileName	HTMLElement		The caption text file to upload. The file must be in UTF-8 encoding. (optional)
+	 **/
+	uploadCaptionFile: function(json, fileName){
+		var kparams = new Object();
+		var kfiles = new Object();
+		kparams.json = json;
+		kfiles.fileName = fileName;
+		return new KalturaRequestBuilder("assetenrich", "uploadCaptionFile", kparams, kfiles);
+	}
+}
+
+/**
  *Class definition for the Kaltura service: assetFile.
  **/
 var KalturaAssetFileService = {
@@ -8423,7 +8493,7 @@ var MD5 = function (string) {
 function KalturaClient(config){
 	this.init(config);
 	this.setClientTag('ajax:25-01-07');
-	this.setApiVersion('10.7.1.4');
+	this.setApiVersion('10.8.0.0');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
