d108fac64abfdb651c5085dc4c73666b33814376
Backend CI <ott.rnd.core@kaltura.com>
2025-07-02 07:01:38 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1516, branch 11.4.0.0

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -2762,6 +2762,61 @@ var KalturaFollowTvSeriesService = {
 }
 
 /**
+ *Class definition for the Kaltura service: geoBlockRule.
+ **/
+var KalturaGeoBlockRuleService = {
+	/**
+	 * Add a new geo block rule.
+	 * @param	geoBlockRule	KalturaGeoBlockRule		The geo block rule to add (optional)
+	 **/
+	add: function(geoBlockRule){
+		var kparams = new Object();
+		kparams.geoBlockRule = geoBlockRule;
+		return new KalturaRequestBuilder("geoblockrule", "add", kparams);
+	},
+	
+	/**
+	 * Delete a geo block rule.
+	 * @param	id	int		The id of the geo block rule to delete (optional)
+	 **/
+	deleteAction: function(id){
+		var kparams = new Object();
+		kparams.id = id;
+		return new KalturaRequestBuilder("geoblockrule", "delete", kparams);
+	},
+	
+	/**
+	 * Get the list of geo block rules for the partner.
+	 * @param	filter	KalturaGeoBlockRuleFilter		Filter criteria for the geo block rules (optional, default: null)
+	 * @param	pager	KalturaFilterPager		Paging information for retrieving paginated results (optional, default: null)
+	 **/
+	listAction: function(filter, pager){
+		if(!filter)
+			filter = null;
+		if(!pager)
+			pager = null;
+		var kparams = new Object();
+		if (filter != null)
+			kparams.filter = filter;
+		if (pager != null)
+			kparams.pager = pager;
+		return new KalturaRequestBuilder("geoblockrule", "list", kparams);
+	},
+	
+	/**
+	 * Update an existing geo block rule.
+	 * @param	id	int		The id of the geo block rule to update (optional)
+	 * @param	geoBlockRule	KalturaGeoBlockRule		The geo block rule data to update (optional)
+	 **/
+	update: function(id, geoBlockRule){
+		var kparams = new Object();
+		kparams.id = id;
+		kparams.geoBlockRule = geoBlockRule;
+		return new KalturaRequestBuilder("geoblockrule", "update", kparams);
+	}
+}
+
+/**
  *Class definition for the Kaltura service: homeNetwork.
  **/
 var KalturaHomeNetworkService = {
@@ -8624,8 +8679,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:25-06-11');
-	this.setApiVersion('11.2.1.0');
+	this.setClientTag('ajax:25-07-02');
+	this.setApiVersion('11.4.0.0');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
