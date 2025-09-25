292f2f2fbfceed2724802fe51c635cab57548b01
Backend CI <ott.rnd.core@kaltura.com>
2025-09-25 08:43:31 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1555, branch 11_7_0

--- KalturaFullClient.js
+++ KalturaFullClient.js
@@ -631,7 +631,7 @@ var KalturaAssetFilePpvService = {
 	},
 	
 	/**
-	 * Update assetFilePpv.
+	 * Update assetFilePpv dates.
 	 * @param	assetFileId	int		Asset file id (optional)
 	 * @param	ppvModuleId	int		Ppv module id (optional)
 	 * @param	assetFilePpv	KalturaAssetFilePpv		assetFilePpv (optional)
@@ -8763,8 +8763,8 @@ var MD5 = function (string) {
  */
 function KalturaClient(config){
 	this.init(config);
-	this.setClientTag('ajax:25-08-06');
-	this.setApiVersion('11.5.0.0');
+	this.setClientTag('ajax:25-09-25');
+	this.setApiVersion('11.6.0.1');
 }
 KalturaClient.inheritsFrom (KalturaClientBase);
 /**
